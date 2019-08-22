e8273f38d8c6ad884b46f60485d21044fa88f9f7
Taylor Page <jonathantaylorpage@gmail.com>
2019-08-22 15:21:36 -0700

    (feat) Allow key presses to be registered on midi
    - Leverage onmidimessage to register key presses
    
    Signed-off-by: Taylor Page <jontaylorpage@gmail.com>

--- src/midiAccess.js
+++ src/midiAccess.js
@@ -1,10 +1,26 @@
-const requestMIDIAccess = () => {
+const requestMIDIAccess = ( midiMessage, stateChange) => {
   navigator.requestMIDIAccess()
     .then( access => {
+      Array.from( access.inputs.values() ).map( e => {
+        e.onmidimessage = ( m ) => {
+          const [command, key, velocity] = m.data;
+          console.log( command, keyMap[ key ], velocity );
+          midiMessage && midiMessage();
+        }
+        return true;
+      });
       access.onstatechange = ( e ) => {
         console.log( e.port.name, e.port.manufacturer, e.port.state );
+        stateChange && stateChange();
       };
     });
+}
+
+const keyMap = [];
+const keys = [ 'C', 'C#/Db', 'D', 'D#/Eb', 'E', 'F', 'F#/Gb', 'G', 'G#/Ab', 'A', 'A#/Bb', 'B', ]
+
+for ( let i=0; i<11; i++ ) {
+  keys.forEach( key => keyMap.push( key ));
 }
 
 export default requestMIDIAccess;

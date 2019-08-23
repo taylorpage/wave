7d2b2a0967d561cb2e694c605511a739bbcee0c0
Taylor Page <jonathantaylorpage@gmail.com>
2019-08-23 15:27:13 -0700

    (feat) Allow key color to change on click
    - store pressed key ids in the state
    - create dynamic classes to change key style
    
    Signed-off-by: Taylor Page <jontaylorpage@gmail.com>

--- src/components/Keyboard/Keyboard.js
+++ src/components/Keyboard/Keyboard.js
@@ -5,6 +5,7 @@ import maps from '../../keyMaps';
 class Keyboard extends React.Component {
   constructor() {
     super();
+    this.state = { pressedKeys: {} };
     this.whiteKeys = [];
     this.hiddenKeys = [ 2, 6, 9, 13, 16, 17 ];
     this.createWhiteKeys();
@@ -20,10 +21,22 @@ class Keyboard extends React.Component {
     const freqRef = maps.pianoMap[ keyId ];
     const freq = maps.freqMap[ freqRef ];
     this.props.play( keyId, freq );
+    this.setPressedKeys( keyId, true );
   }
 
   handleMouseUp = ( keyId ) => {
     this.props.stop( keyId );
+    this.setPressedKeys( keyId, false );
+  }
+
+  setPressedKeys( key, status ) {
+    const pressedKeys = Object.assign(
+      this.state.pressedKeys,
+      { [ key ]: status }
+    );
+    this.setState({
+      pressedKeys: pressedKeys
+    });
   }
 
   render() {
@@ -37,7 +50,10 @@ class Keyboard extends React.Component {
                   return (
                     <th key={ `black-${ i }` }>
                       <div
-                        className={ `key black ${ this.hiddenKeys.indexOf( i ) > -1 && 'hidden' }` }
+                        className={
+                          `key black ${ this.hiddenKeys.indexOf( i ) > -1 ? 'hidden' : '' }` +
+                          ( this.state.pressedKeys[ `b-${ i }` ] ? ' pressed' : '' )
+                        }
                         onMouseDown={ this.handleMouseDown.bind( this, `b-${ i }` ) }
                         onMouseUp={ this.handleMouseUp.bind( this, `b-${ i }` ) }
                       ></div>
@@ -56,7 +72,7 @@ class Keyboard extends React.Component {
                   return (
                     <th key={ `white-${ i }` }>
                       <div
-                        className="key white"
+                        className={ `key white ${ this.state.pressedKeys[ `w-${ i }` ] ? ' pressed' : '' }` }
                         onMouseDown={ this.handleMouseDown.bind( this, `w-${ i }` ) }
                         onMouseUp={ this.handleMouseUp.bind( this, `w-${ i }` ) }
                       ></div>
